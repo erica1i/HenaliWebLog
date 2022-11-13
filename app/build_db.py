0877ecd4bef97d770f5bfb497035f9c7dfe62684
erica1i <eli30@stuy.edu>
2022-11-13 15:39:38 -0500

    aesthetic changes

--- app/build_db.py
+++ app/build_db.py
@@ -4,10 +4,11 @@ db_name = "test.db"
 
 db = sqlite3.connect(db_name)
 c =  db.cursor()
+
 #DROP TABLE IF EXISTS users;
 c.execute("CREATE TABLE users (name TEXT PRIMARY KEY, passwd TEXT);")
+c.execute("CREATE TABLE names (user_name TEXT PRIMARY KEY, name TEXT);")
 c.execute("CREATE TABLE blogs (name TEXT, user_name TEXT, id INTEGER PRIMARY KEY);")
-c.execute("CREATE TABLE entries (name TEXT, contents TEXT, blog_id INT, id INTEGER PRIMARY KEY);")
 
 db.commit()
 db.close()
